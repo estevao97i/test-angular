bc4cb9d1cf7708b1a600859d1144707e9b03447f
ëstevao97i <pedro.cordova2002@gmail.com>
2024-07-02 19:14:47 -0300

    evento de clique triggered

--- curso-angular-testes-unitarios-template-main/src/app/fill-form/fill-form.component.spec.ts
+++ curso-angular-testes-unitarios-template-main/src/app/fill-form/fill-form.component.spec.ts
@@ -2,6 +2,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FillFormComponent } from './fill-form.component';
+import { By } from '@angular/platform-browser';
 
 describe('FillFormComponent', () => {
   let component: FillFormComponent;
@@ -22,4 +23,13 @@ describe('FillFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve disparar um evento de input', () => {
+    let input = fixture.debugElement.query(By.css('input')).nativeElement;
+
+    input.value = 'estevo';
+    input.dispatchEvent(new Event('input'));
+
+    expect(input.value).toBe('estevo')
+  })
 });
