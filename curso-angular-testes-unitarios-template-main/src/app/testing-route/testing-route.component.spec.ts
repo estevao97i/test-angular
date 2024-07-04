a1dd6a4622b415f9874ce5f7db0e8f1c15031156
ëstevao97i <pedro.cordova2002@gmail.com>
2024-07-04 16:13:01 -0300

    testando rotas

--- curso-angular-testes-unitarios-template-main/src/app/testing-route/testing-route.component.spec.ts
+++ curso-angular-testes-unitarios-template-main/src/app/testing-route/testing-route.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestingRouteComponent } from './testing-route.component';
+import { Router } from '@angular/router';
 
 describe('TestingRouteComponent', () => {
   let component: TestingRouteComponent;
   let fixture: ComponentFixture<TestingRouteComponent>;
+  let router: Router
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ TestingRouteComponent ]
@@ -14,10 +17,18 @@ describe('TestingRouteComponent', () => {
     fixture = TestBed.createComponent(TestingRouteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve testar se rota está sendo chamada para o local certo', () => {
+    let route = 'extrato';
+    spyOn(router, 'navigate');
+    component.navigateUrl(route);
+    expect(router.navigate).toHaveBeenCalledWith(['/extrato']);
+  })
 
 });
