05baa3cab36e21ecda65d4af2fd4494ecc91befe
ëstevao97i <pedro.cordova2002@gmail.com>
2024-07-04 16:04:42 -0300

    testes de pipe e guard feitos

--- curso-angular-testes-unitarios-template-main/src/app/guard.guard.spec.ts
+++ curso-angular-testes-unitarios-template-main/src/app/guard.guard.spec.ts
@@ -1,17 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing'
 import { GuardGuard } from './guard.guard';
+import { Router } from '@angular/router';
 
 describe('GuardGuard', () => {
   let guard: GuardGuard;
+  let routeMock: any = { snapshot: {} }
+  let routeStateMock: any = { snapshot: {}, url: ['/login'] }
+  let routerMock = { navigate: jasmine.createSpy('navigate') }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Router, useValue: routerMock
+        }
+      ],
+      imports: [ RouterTestingModule ]
+    });
     guard = TestBed.inject(GuardGuard);
+    localStorage.removeItem('token');
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
 
+  it('deve redirecionar usuario para tela de login quando não tiver token', () => {
+    expect(guard.canActivate(routeMock, routeStateMock)).toEqual(false);
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
+  })
+
+  it('deve deixar o usuario entrar na rota caso tenha token', () => {
+    localStorage.setItem('token', 'asdafdsfsdf');
+    expect(guard.canActivate(routeMock, routeStateMock)).toEqual(true);
+  })
 });
--- curso-angular-testes-unitarios-template-main/src/app/http.pipe.spec.ts
+++ curso-angular-testes-unitarios-template-main/src/app/http.pipe.spec.ts
@@ -6,4 +6,9 @@ describe('HttpPipe', () => {
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
+
+  it('testing pipe http()', () => {
+    expect(pipe.transform('http://google.com')).toBe('https://google.com');
+  })
+  
 });
